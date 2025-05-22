56092ed274c269bde3e8dfb3c66a30e98e2d83c6
ViSiOnOp19cr <chandancr98195@gmail.com>
2025-05-22 21:55:43 +0530

    added baic project.

---
+++ packages/utils/index.ts
@@ -0,0 +1,3 @@
+export const summarizeNote = (note: string) => `Summarize this note: ${note}`;
+export const quizFromNote = (note: string) => `Create a quiz from this: ${note}`;
+export const simplifyNote = (note: string) => `Explain this simply: ${note}`;
